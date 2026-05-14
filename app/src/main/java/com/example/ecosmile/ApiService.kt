de26ae2de95bfc7eb4915ae094ec7f9ab15551f4
Gustavo Oliveira <gustavoa723@gmail.com>
2026-05-14 01:23:00 -0300

    feat: adicionar métodos e rotas de administrador no ApiService.kt
    - Rotas de administrador para operações CRUD adicionadas ao arquivo ApiService.kt
    - Comunicação das novas rotas com a Api externa

--- app/src/main/java/com/example/ecosmile/ApiService.kt
+++ app/src/main/java/com/example/ecosmile/ApiService.kt
@@ -31,4 +31,52 @@ interface ApiService {
         @Field("email") email: String,
         @Field("nova_senha") novaSenha: String
     ): Call<Void>
+
+    // Rotas de admin
+
+    // login
+    @GET("login_admin.php")
+    fun loginAdmin(
+        @Query("email") email: String,
+        @Query("senha") senha: String
+    ): Call<List<AdminResponse>>
+
+    // tipo de consulta
+    @FormUrlEncoded
+    @POST("criar_tipo_consulta.php")
+    fun criarTipoConsulta(
+        @Field("descricao") descricao: String,
+        @Field("unidade") unidade: String,
+        @Field("valor") valor: String
+    ): Call<Void>
+
+    // tipo de desconto
+    @FormUrlEncoded
+    @POST("criar_tipo_desconto.php")
+    fun criarTipoDesconto(
+        @Field("descricao") descricao: String,
+        @Field("expira") expira: String,
+        @Field("valor") valor: String
+    ): Call<Void>
+
+    // ponto de coleta
+    @FormUrlEncoded
+    @POST("criar_ponto_coleta.php")
+    fun criarPontoColeta(
+        @Field("nome") nome: String,
+        @Field("endereco") endereco: String,
+        @Field("latitude") latitude: String,
+        @Field("longitude") longitude: String,
+        @Field("horario") horario: String
+    ): Call<Void>
+
+    // token
+    @GET("token.php")
+    fun listarTokens(): Call<List<TokenResponse>>
+
+    @FormUrlEncoded
+    @POST("token.php")
+    fun gerarToken(
+        @Field("tipoDesconto_ID") tipoDescontoId: String
+    ): Call<TokenGeradoResponse>
 }
